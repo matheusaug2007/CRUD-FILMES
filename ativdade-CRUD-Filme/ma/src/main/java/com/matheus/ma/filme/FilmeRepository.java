302f459a4f2622375c2aa360f6cdf2bed0537609
Matheus <matheus.augusto5117@gmail.com>
2026-09-08 19:51:31 -0300

    Delete ativdade-CRUD-Filme directory

--- ativdade-CRUD-Filme/ma/src/main/java/com/matheus/ma/filme/FilmeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.matheus.ma.filme;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FilmeRepository extends JpaRepository<FilmeModel, Long> {
-
-}
